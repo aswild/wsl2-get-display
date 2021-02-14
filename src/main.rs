d0bd047d5eebbe74fd35d486974c02975887a1a9
Allen Wild <allenwild93@gmail.com>
2021-02-13 21:11:22 -0500

    inline get_host_ip and combine it with IpAddr parsing
    Chaining is too much fun

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 // But since I gotta shell out to another binary anyway, I might as well add the /etc/resolv.conf
 // parsing logic here too.
 
+use std::fs;
 use std::io::ErrorKind;
 use std::net::{IpAddr, SocketAddr, TcpStream};
 use std::num::ParseIntError;
@@ -13,20 +14,6 @@ use anyhow::{anyhow, Context, Result};
 use structopt::StructOpt;
 
 const DISPLAY_PORT_OFFSET: u16 = 6000;
-
-fn get_host_ip() -> Result<String> {
-    String::from_utf8(std::fs::read("/etc/resolv.conf").context("failed to read /etc/resolv.conf")?)
-        .context("/etc/resolv.conf isn't valid utf8")?
-        .lines()
-        .find_map(|line| {
-            let mut words = line.split_ascii_whitespace();
-            match (words.next(), words.next()) {
-                (Some("nameserver"), Some(addr)) => Some(addr.to_owned()),
-                (_, _) => None,
-            }
-        })
-        .ok_or_else(|| anyhow!("unable to get host IP address from /etc/resolv.conf"))
-}
 
 fn parse_duration(s: &str) -> Result<Duration, ParseIntError> {
     Ok(Duration::from_millis(s.parse()?))
@@ -53,13 +40,23 @@ struct Args {
 fn run() -> Result<Option<String>> {
     let args = Args::from_args();
 
-    // read /etc/resolv.conf and find the first nameserver
-    let host_ip = get_host_ip()?;
+    // read /etc/resolv.conf, find the first nameserver, and parse it as an ip address
+    let host_ip =
+        String::from_utf8(fs::read("/etc/resolv.conf").context("failed to read /etc/resolv.conf")?)
+            .context("/etc/resolv.conf isn't valid utf8")?
+            .lines()
+            .find_map(|line| {
+                let mut words = line.split_ascii_whitespace();
+                match (words.next(), words.next()) {
+                    (Some("nameserver"), Some(addr)) => Some(addr.to_owned()),
+                    (_, _) => None,
+                }
+            })
+            .ok_or_else(|| anyhow!("unable to find host IP address in /etc/resolv.conf"))?
+            .parse::<IpAddr>()
+            .context("unable to parse host IP address")?;
 
-    let sa = SocketAddr::new(
-        host_ip.parse::<IpAddr>().context("invalid host IP address")?,
-        DISPLAY_PORT_OFFSET + args.display_number,
-    );
+    let sa = SocketAddr::new(host_ip, DISPLAY_PORT_OFFSET + args.display_number);
 
     match TcpStream::connect_timeout(&sa, args.timeout) {
         // yay we connected. the socket will be closed when it goes out of scope and is dropped
