391f2986d5c8e8d7084b47863d2dbc543df51a41
Allen Wild <allenwild93@gmail.com>
2021-02-13 21:58:04 -0500

    add retry count and debug logging
    also check for overflow of the port number

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,32 @@ use std::io::ErrorKind;
 use std::net::{IpAddr, SocketAddr, TcpStream};
 use std::num::ParseIntError;
 use std::process::exit;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::thread::sleep;
 use std::time::Duration;
 
 use anyhow::{anyhow, Context, Result};
 use structopt::StructOpt;
 
+/// X11 port number is 6000 plus the display number
 const DISPLAY_PORT_OFFSET: u16 = 6000;
+
+/// Lazy global-variable debug logging. Use AtomicBool because accessing static mut variables is
+/// unsafe even though we're single-threaded.
+static DEBUG: AtomicBool = AtomicBool::new(false);
+macro_rules! debug {
+    ($($args:tt),+) => {
+        if DEBUG.load(Ordering::Relaxed) {
+            eprintln!($($args),+);
+        }
+    };
+}
 
 fn parse_duration(s: &str) -> Result<Duration, ParseIntError> {
     Ok(Duration::from_millis(s.parse()?))
 }
 
-/// Find an X server running on the WSL2 host, and return a value for the DISPLAY environment
-/// variable.
+/// Find an X server running on the WSL2 host.
 ///
 /// wsl-get-display will infer the WSL2 hypervisor IP by finding the first nameserver in
 /// /etc/resolv.conf, then attempts a TCP connection on the appropriate port (6000
@@ -28,9 +41,17 @@ fn parse_duration(s: &str) -> Result<Duration, ParseIntError> {
 #[derive(Debug, StructOpt)]
 #[structopt(max_term_width = 80)]
 struct Args {
-    /// connection timeout in milliseconds
+    /// Connection timeout in milliseconds
     #[structopt(short, long, parse(try_from_str = parse_duration), default_value = "500")]
     timeout: Duration,
+
+    /// Number of retries
+    #[structopt(short, long, default_value = "1")]
+    retries: u16,
+
+    /// Enables verbose debug output on stderr
+    #[structopt(short, long)]
+    verbose: bool,
 
     /// X display number, e.g. the "1" in "localhost:1"
     #[structopt(default_value = "1")]
@@ -39,6 +60,7 @@ struct Args {
 
 fn run() -> Result<Option<String>> {
     let args = Args::from_args();
+    DEBUG.store(args.verbose, Ordering::Relaxed);
 
     // read /etc/resolv.conf, find the first nameserver, and parse it as an ip address
     let host_ip =
@@ -56,18 +78,38 @@ fn run() -> Result<Option<String>> {
             .parse::<IpAddr>()
             .context("unable to parse host IP address")?;
 
-    let sa = SocketAddr::new(host_ip, DISPLAY_PORT_OFFSET + args.display_number);
+    let port = DISPLAY_PORT_OFFSET
+        .checked_add(args.display_number)
+        .ok_or_else(|| anyhow!("display offset overflowed max port number"))?;
 
-    match TcpStream::connect_timeout(&sa, args.timeout) {
-        // yay we connected. the socket will be closed when it goes out of scope and is dropped
-        Ok(_) => Ok(Some(format!("{}:{}", host_ip, args.display_number))),
-        // hide error messages for timeouts or connection refused or whatever
-        Err(e) if matches!(e.kind(), ErrorKind::ConnectionRefused | ErrorKind::TimedOut) => {
-            Ok(None)
+    let sa = SocketAddr::new(host_ip, port);
+    debug!("connecting to {}", sa);
+
+    for retry in 1..=args.retries {
+        debug!("connect attempt {}", retry);
+        match TcpStream::connect_timeout(&sa, args.timeout) {
+            Ok(conn) => {
+                debug!("connection succeeded: {:?}", conn);
+                return Ok(Some(format!("{}:{}", host_ip, args.display_number)));
+                // conn goes out of scope and is dropped, closing the connection
+            }
+
+            Err(e) => {
+                debug!("connection failed: {}", e);
+                match e.kind() {
+                    // timeout, retry immediately
+                    ErrorKind::TimedOut => (),
+                    // connection refused, wait for timeout before retrying
+                    ErrorKind::ConnectionRefused => sleep(args.timeout),
+                    // bail on any other errors
+                    _ => return Err(e.into()),
+                }
+            }
         }
-        // some other error, print that out
-        Err(e) => Err(e.into()),
     }
+
+    debug!("retries exhausted, no server found");
+    Ok(None)
 }
 
 fn main() {
