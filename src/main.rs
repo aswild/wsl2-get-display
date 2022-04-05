56ef04955971b2ae9be289b94099e913787e55a9
Allen Wild <allenwild93@gmail.com>
2022-04-05 16:52:40 -0400

    version 1.1.0: parse "ip route show default" instead of /etc/resolv.conf
    Finally got WSL2 at work, but its resolv.conf doesn't have search
    domains, which means I'll probably have to manually write
    /etc/resolv.conf which means I can't rely on it to be a source of truth
    about the host/hypervisor IP.
    
    Instead, I can figure that out by parsing ip-route's output to get the
    default gateway, since it seems like it should be the same thing (at
    least in most cases, assuming there's not some weird container or VPN
    stuff going on).
    
    Bring in serde_json and parse `ip -4 -json route show default` to get
    the default IP. The old method is still available with the
    -R/--resolv-conf flag if needed.

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,17 @@
 // parsing logic here too.
 
 use std::fs;
-use std::io::ErrorKind;
+use std::io::{Cursor, ErrorKind};
 use std::net::{IpAddr, SocketAddr, TcpStream};
 use std::num::ParseIntError;
-use std::process::exit;
+use std::process::{exit, Command, Stdio};
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::thread::sleep;
 use std::time::Duration;
 
-use anyhow::{anyhow, Context, Result};
+use anyhow::{anyhow, bail, ensure, Context, Result};
 use clap::Parser;
+use serde_json::{self, Value};
 
 /// X11 port number is 6000 plus the display number
 const DISPLAY_PORT_OFFSET: u16 = 6000;
@@ -32,13 +33,92 @@ fn parse_duration(s: &str) -> Result<Duration, ParseIntError> {
     Ok(Duration::from_millis(s.parse()?))
 }
 
+/// Determine the host/hypervisor IP by reading the first nameserver from /etc/resolv.conf
+///
+/// This is what most basic answers/tutorials online suggest, and it's fine in a default
+/// configuration, but won't work in WSL setups that use a custom resolv.conf (e.g. when needing to
+/// add search domains or something, or for any other reason don't use the host as WSL's DNS)
+fn host_ip_from_resolv_conf() -> Result<IpAddr> {
+    String::from_utf8(fs::read("/etc/resolv.conf").context("failed to read /etc/resolv.conf")?)
+        .context("/etc/resolv.conf isn't valid utf8")?
+        .lines()
+        .find_map(|line| {
+            let mut words = line.split_ascii_whitespace();
+            match (words.next(), words.next()) {
+                (Some("nameserver"), Some(addr)) => Some(addr.to_owned()),
+                (_, _) => None,
+            }
+        })
+        .ok_or_else(|| anyhow!("unable to find host IP address in /etc/resolv.conf"))?
+        .parse::<IpAddr>()
+        .context("unable to parse host IP address")
+}
+
+/// Determine the host/hypervisor IP by getting the default IPv4 route.
+///
+/// This parses `ip -4 -json route show default` and extracts the gateway IP address. It should be
+/// more reliable than the /etc/resolv.conf method, but could still fail in case a VPN client is
+/// running inside the WSL VM or something like that.
+///
+/// Tested on iproute2 v5.9.0 on ubuntu 21.10. I think the json flag was added in v4.17 which was
+/// released in mid-2018, so a somewhat recent distro is needed.
+fn host_ip_from_route() -> Result<IpAddr> {
+    let mut cmd = Command::new("ip");
+    cmd.args(&["-4", "-json", "route", "show", "default"])
+        .stdout(Stdio::piped())
+        .stderr(Stdio::inherit());
+    let output = cmd.output().context("failed to execute {cmd:?}")?;
+    if !output.status.success() {
+        bail!("command {cmd:?} failed");
+    }
+
+    // The JSON output should like this. Parse manually rather than pulling in serde derive macros.
+    // [
+    //   {
+    //     "dst": "default",
+    //     "gateway": "172.30.192.1",
+    //     "dev": "eth0",
+    //     "flags": []
+    //   }
+    // ]
+    let js: Value = serde_json::from_reader(Cursor::new(&output.stdout))
+        .context("failed to parse output as JSON")?;
+    debug!("{cmd:?} returned parsed data '{js}'");
+
+    // unwrap inner object out of outer array
+    let js = match js {
+        Value::Array(mut values) => {
+            if values.is_empty() {
+                bail!("empty json array");
+            } else if values.len() > 1 {
+                eprintln!("warning: ip route returned multiple defaults routes: {values:?}");
+            }
+            values.remove(0)
+        }
+        not_an_array => bail!("expected JSON array, got {not_an_array}"),
+    };
+
+    // sanity check, "dst" field should be "default"
+    ensure!(
+        matches!(js["dst"].as_str(), Some("default")),
+        "route destination is not 'default': {js}"
+    );
+
+    // extract and parse the gateway field as an IP
+    js["gateway"]
+        .as_str()
+        .ok_or_else(|| anyhow!("default gateway not found (or is not a string)"))?
+        .parse::<IpAddr>()
+        .context("failed to parse default gateway IP address")
+}
+
 /// Find an X server running on the WSL2 host.
 ///
 /// wsl-get-display will infer the WSL2 hypervisor IP by finding the first nameserver in
 /// /etc/resolv.conf, then attempts a TCP connection on the appropriate port (6000
 /// + display_number)
 #[derive(Debug, Parser)]
-#[clap(max_term_width = 80)]
+#[clap(version, max_term_width = 80)]
 struct Args {
     /// Connection timeout in milliseconds
     #[clap(short, long, parse(try_from_str = parse_duration), default_value = "500")]
@@ -55,6 +135,11 @@ struct Args {
     /// X display number, e.g. the "1" in "localhost:1"
     #[clap(default_value = "1")]
     display_number: u16,
+
+    /// Use /etc/resolv.conf to determine the host IP address rather than parsing the output of
+    /// `ip route`
+    #[clap(short = 'R', long)]
+    resolv_conf: bool,
 }
 
 fn run() -> Result<Option<String>> {
@@ -62,20 +147,7 @@ fn run() -> Result<Option<String>> {
     DEBUG.store(args.verbose, Ordering::Relaxed);
 
     // read /etc/resolv.conf, find the first nameserver, and parse it as an ip address
-    let host_ip =
-        String::from_utf8(fs::read("/etc/resolv.conf").context("failed to read /etc/resolv.conf")?)
-            .context("/etc/resolv.conf isn't valid utf8")?
-            .lines()
-            .find_map(|line| {
-                let mut words = line.split_ascii_whitespace();
-                match (words.next(), words.next()) {
-                    (Some("nameserver"), Some(addr)) => Some(addr.to_owned()),
-                    (_, _) => None,
-                }
-            })
-            .ok_or_else(|| anyhow!("unable to find host IP address in /etc/resolv.conf"))?
-            .parse::<IpAddr>()
-            .context("unable to parse host IP address")?;
+    let host_ip = if args.resolv_conf { host_ip_from_resolv_conf() } else { host_ip_from_route() }?;
 
     let port = DISPLAY_PORT_OFFSET
         .checked_add(args.display_number)
